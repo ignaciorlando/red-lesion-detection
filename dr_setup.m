ef431a1b7b99fc81bd93661e5eb24753343f209c
ignaciorlando <ignaciorlando@gmail.com>
2016-11-18 14:04:56 -0300

    Warnings on dr_setup added

--- dr_setup.m
+++ dr_setup.m
@@ -13,30 +13,46 @@ addpath(genpath(fullfile(root,'Util','eval'))) ;
 addpath(genpath(fullfile(root,'Util','files'))) ;
 addpath(genpath(fullfile(root,'Util','images'))) ;
 addpath(genpath(fullfile(root,'Util','labels'))) ;
+
 % add external libraries
+if exist(fullfile(root,'Util','external'), 'dir')==0
+    mkdir(fullfile(root,'Util','external'));
+end
 addpath(fullfile(root,'Util','external')) ;
-addpath(genpath(fullfile(root,'Util','external','hline_vline'))) ; % code for vertical line
-addpath(genpath(fullfile(root,'Util','external','markSchmidt'))) ; % code for logistic regression
-addpath(genpath(fullfile(root,'Util','external','mseb'))) ; % code for nice error bars
-addpath(genpath(fullfile(root,'Util','external','alphamask'))) ; % code for nice MAs
-addpath(genpath(fullfile(root,'Util','external','RF_Class_C'))) ; % code for random forests
-addpath(fullfile(root,'Util','external','vlfeat','toolbox')) ; % code for ROC curves and stuff
-addpath(fullfile(root,'Util','external','matconvnet-master', 'matlab')) ; % code for CNN learning
-    
 
-% setup vl_feat
-vl_setup;
-% setup matconvnet
-vl_setupnn;
+% if RF does not exist, show a warning message
+if exist(fullfile(root,'Util','external','RF_Class_C'), 'dir')==0
+    warning('We could not find RF_Class_C. Please, download the package from here: https://github.com/PetterS/hep-2/tree/master/randomforest-matlab/RF_Class_C');
+else
+    addpath(genpath(fullfile(root,'Util','external','RF_Class_C'))) ; % code for random forests
+end
 
-root = vl_rootnn() ;
+% if VLFeat does not exist, show a warning message
+if exist(fullfile(root,'Util','external','vlfeat','toolbox'), 'dir')==0
+    warning('We could not find VLFeat. Please, download the package from here: http://www.vlfeat.org/download.html');
+else
+    addpath(fullfile(root,'Util','external','vlfeat','toolbox')) ; % code for ROC curves and stuff
+    % setup vl_feat
+    vl_setup;
+end
 
-original_simplenn_filename = fullfile(root, 'matlab', 'simplenn', 'vl_simplenn.m*');
-if numel(dir(original_simplenn_filename')) ~= 0
-    delete(original_simplenn_filename);
+% if Matconvnet does not exist, show a warning message
+if exist(fullfile(root,'Util','external','matconvnet-master', 'matlab'))==0
+    warning('We could not find Matconvnet. Please, download the package from here: https://github.com/vlfeat/matconvnet/');
+else
+    addpath(fullfile(root,'Util','external','matconvnet-master', 'matlab')) ; % code for CNN learning
+    % setup matconvnet
+    vl_setupnn;
+    root = vl_rootnn() ;
+
+    original_simplenn_filename = fullfile(root, 'matlab', 'simplenn', 'vl_simplenn.m*');
+    if numel(dir(original_simplenn_filename')) ~= 0
+        delete(original_simplenn_filename);
+    end
+
+    if numel(dir(fullfile(root, 'matlab', 'mex', 'vl_nnconv.mex*'))) == 0
+        % compile MEX files
+        vl_compilenn;
+    end
 end
     
-if numel(dir(fullfile(root, 'matlab', 'mex', 'vl_nnconv.mex*'))) == 0
-    % compile MEX files
-    vl_compilenn;
-end
