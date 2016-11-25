958cfad1ec0f0c0bc5026c9be3567fa626795c2e
ignaciorlando <ignaciorlando@gmail.com>
2016-11-25 11:03:17 -0300

    Minor change in get_hand_crafted_training_set

--- RedLesionDetection/util/deep_learning/get_hand_crafted_training_set.m
+++ RedLesionDetection/util/deep_learning/get_hand_crafted_training_set.m
@@ -38,3 +38,26 @@ function [training_ma_features, training_ma_labels, validation_ma_features, vali
         splits.validationIndices, ma_features_training, ma_labels_training);
     
 end
+
+function [splits, sorting] = train_val_splits(sizeSet, sorting, options)
+
+    % If the parameter sorting is an empty array, generate an array of 
+    % numbers from 1 to sizeSet to represent the indices, and permute it
+    % randomly
+    indices = 1:1:sizeSet;
+    if (isempty(sorting))
+        sorting = indices(randperm(length(indices)));
+    end
+    
+    splits = cell(1,1);
+
+    % The validation size will be equal to 
+    validationSize = floor((1 - options.trainingFraction) * sizeSet);
+
+    % The remaining images will correspond to the training data
+    splits{1}.validationIndices = sorting(1:validationSize);
+    
+    % The remaining images will correspond to the training data
+    splits{1}.trainingIndices = sorting(validationSize+1:end);
+
+end
