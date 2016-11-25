8c6eb4fb49f0cc300a543df47236ece56395336c
ignaciorlando <ignaciorlando@gmail.com>
2016-11-25 11:02:22 -0300

    Tons of useless functions removed

--- Learning/configuration/cumulateData.m
+++
@@ -1,6 +0,0 @@
-function [cumulateHere] = cumulateData(cumulateHere, cumulateThis)
-    cumulateHere.features = cat(1, cumulateHere.features, cumulateThis.features);
-    cumulateHere.pre_bag_features = [cumulateHere.pre_bag_features; cumulateThis.pre_bag_features];
-    cumulateHere.indices = cat(2, cumulateHere.indices, cumulateThis.indices);
-    cumulateHere.labels = cat(1, cumulateHere.labels, cumulateThis.labels);
-end
