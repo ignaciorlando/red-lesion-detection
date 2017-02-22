4e04794ae02f9facce125907004916f1b5c380e3
ignaciorlando <ignaciorlando@gmail.com>
2017-02-22 11:47:12 -0300

    Code to plot red lesion probabilities

---
+++ RedLesionDetection/util/display/imshow_red_lesion_prob.m
@@ -0,0 +1,27 @@
+function our_figure = imshow_red_lesion_prob(I, probability_map)
+
+    % Retrieve each centroid
+    properties = regionprops(probability_map > 0, 'centroid', 'PixelIdxList');
+    
+    % Show the image
+    figure, imshow(I);
+    hold on
+    for i = 1 : length(properties)
+
+        % create a binary mask with only the current lesion
+        bw_mask = false(size(I,1), size(I,2));
+        bw_mask(properties(i).PixelIdxList) = true;
+        
+        % retrieve probability
+        current_probabilities = unique(probability_map(properties(i).PixelIdxList));
+        
+        % now incorporate it to current figure
+        alphamask(bw_mask, [0 0 1], current_probabilities(1), gca);
+            
+    end
+    
+    hold off
+
+    our_figure = frame2im(getframe(gcf)); 
+
+end
