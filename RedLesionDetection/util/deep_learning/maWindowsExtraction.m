a985fe0f8686292fa267672a6adb228d987a8a81
ignaciorlando <ignaciorlando@gmail.com>
2017-04-20 14:49:51 -0300

    Computation of the windows now take into account the size of the lesion

--- RedLesionDetection/util/deep_learning/maWindowsExtraction.m
+++ RedLesionDetection/util/deep_learning/maWindowsExtraction.m
@@ -14,31 +14,61 @@ function [ma_windows, candidate_coordinates] = maWindowsExtraction(ma_candidates
     mask = mask > 0;
     % Preprocess the image
     [I] = contrastEqualization(I, mask);
-    % Padd the image with zeros to recover MA candidates windows
-    I_padded = padarray(I, [ceil(window_size/2) ceil(window_size/2)]);
     % Get ma candidates
     conn = bwconncomp(ma_candidates);
-    % Initialize windows mask matrix (only for visualization purposes)
-    windows_masks = zeros(size(I_padded, 1), size(I_padded, 2));
+    
+    % -------------
+    % % Initialize windows mask matrix (only for visualization purposes)
+    %windows_masks = zeros(size(I_padded, 1), size(I_padded, 2));
+    % -------------
     
     % initialize list of windows
     ma_windows = uint8(zeros(window_size, window_size, 3, conn.NumObjects));
     % initialize array of pixel coordinates
     candidate_coordinates = cell(conn.NumObjects, 1);
     % identify centroids
-    stats = regionprops(ma_candidates, 'Centroid', 'PixelIdxList');
+    stats = regionprops(ma_candidates, 'Centroid', 'PixelIdxList','MajorAxisLength');
     
     % for each of the candidates, take a window centered on the MA
     for i = 1 : conn.NumObjects
         
-        center = round(stats(i).Centroid) + window_size/2;
-            
+        % If the major axis length * 2 is smaller than the window size...
+        if (stats(i).MajorAxisLength * 2 < window_size)
+            % This window will have a size equal to the default window size
+            current_window_size = window_size;
+        else
+            % This window will have a size of 2 * its major axis length
+            current_window_size = ceil(stats(i).MajorAxisLength) * 2;
+        end
+        
+        % Localize the central coordinate of the lesion
+        center = round(stats(i).Centroid) + current_window_size/2;
+
+        % Padd the image with zeros to recover MA candidates windows
+        I_padded = padarray(I, [ceil(current_window_size/2) ceil(current_window_size/2)]);
+
+        % Retrieve current window
+        current_window = I_padded(center(2) - current_window_size/2 : center(2) + current_window_size/2 - 1, ...
+                                  center(1) - current_window_size/2 : center(1) + current_window_size/2 - 1, ...
+                                  :);
+        % --------------
+        % %windows_masks(center(2) - current_window_size/2 : center(2) + current_window_size/2 - 1, ...
+        %              center(1) - current_window_size/2 : center(1) + current_window_size/2 - 1) = 1;
+        % --------------
+        
+        % If current_window_size is bigger than the default window size...
+        if current_window_size > window_size
+            % initialize the window
+            my_resized_window = uint8(zeros(window_size, window_size, 3));
+            % resize the window to the original resolution
+            for cb = 1 : size(current_window, 3)
+                my_resized_window(:,:,cb) = imresize(current_window(:,:,cb), [window_size window_size]);
+            end
+            current_window = my_resized_window;
+        end
         % concatenate the new window
-        ma_windows(:,:,:,i) = I_padded(center(2) - window_size/2 : center(2) + window_size/2 - 1, ...
-                                       center(1) - window_size/2 : center(1) + window_size/2 - 1, ...
-                                       :);
-        windows_masks(center(2) - window_size/2 : center(2) + window_size/2 - 1, ...
-                      center(1) - window_size/2 : center(1) + window_size/2 - 1) = 1;
+        ma_windows(:,:,:,i) = current_window;    
+        
         % add the pixel coordinates to the cell array
         candidate_coordinates{i} = conn.PixelIdxList{i};
         
