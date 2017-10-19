dcd607d3484bce76b90aa47ef9d467738b1cd42e
ignaciorlando <ignaciorlando@gmail.com>
2017-10-19 18:00:50 -0300

    Better organization of ROCh data

--- Scripts/data_preparation/script_organize_roch_data.m
+++ Scripts/data_preparation/script_organize_roch_data.m
@@ -11,7 +11,7 @@ config_organize_roch_data
 fprintf('Setting up and creating folders\n');
 
 % set up input folder
-training_input_dir = fullfile(root_folder, 'ROCtraining', 'ROCtraining');
+training_input_dir = fullfile(root_folder, 'ROCtraining');
 test_input_dir = fullfile(root_folder, 'ROCtestImages', 'images');
 
 % set up output folders
@@ -22,24 +22,28 @@ mkdir(training_output_dir_images);
 training_output_dir_labels = fullfile(output_folder, 'ROCh', 'train', 'ma');
 mkdir(training_output_dir_labels);
 % test images
-test_output_dir_images = fullfile(output_folder, 'ROCh', 'test', 'images');
-mkdir(test_output_dir_images);
+if exist(test_input_dir, 'dir')==0
+    test_output_dir_images = fullfile(output_folder, 'ROCh', 'test', 'images');
+    mkdir(test_output_dir_images);
+end
 
 %% copy the files
 fprintf('Copying files\n');
 
-% retrieve all the images in the training and test folders
+% copy all the images on the training set
 training_file_names = dir(fullfile(training_input_dir, '*.jpg'));
 training_file_names = {training_file_names.name};
-test_file_names = dir(fullfile(test_input_dir, '*.jpg'));
-test_file_names = {test_file_names.name};
-
-% copy all the images on the training set
 fprintf('Copying training set images\n');
 copy_roch_files(training_file_names, training_input_dir, training_output_dir_images);
+
 % and on the test set
-fprintf('Copying test set images\n');
-copy_roch_files(test_file_names, test_input_dir, test_output_dir_images);
+if exist(test_input_dir, 'dir')==0
+    test_file_names = dir(fullfile(test_input_dir, '*.jpg'));
+    test_file_names = {test_file_names.name};
+    fprintf('Copying test set images\n');
+    copy_roch_files(test_file_names, test_input_dir, test_output_dir_images);
+end
+
 
 %% generate binary masks using the XML file
 fprintf('Generating labels\n');
@@ -59,10 +63,12 @@ threshold = 0.26;
 GenerateFOVMasks;
 
 % generate FOV masks on test data
-fprintf('Generating FOV masks on test data...\n');
-root = fullfile(output_folder, 'ROCh', 'test');
-threshold = 0.26;
-GenerateFOVMasks;
+if exist(test_input_dir, 'dir')==0
+    fprintf('Generating FOV masks on test data...\n');
+    root = fullfile(output_folder, 'ROCh', 'test');
+    threshold = 0.26;
+    GenerateFOVMasks;
+end
 
 %% crop images, labels and so on
 
@@ -87,21 +93,23 @@ if perform_cropping
     % crop!!
     script_cropFOVSet;
 
-    % cropping training data set
-    fprintf('Cropping test set data...\n');
-    % - path where the images to crop are
-    sourcePaths = { ...
-        fullfile(output_folder, 'ROCh', 'test', 'images'), ...
-        fullfile(output_folder, 'ROCh', 'test', 'masks'), ...
-    };
-    % - paths where the images to be cropped will be saved
-    outputPaths = { ...
-        fullfile(output_folder, 'ROCh', 'test', 'images'), ...
-        fullfile(output_folder, 'ROCh', 'test', 'masks'), ...    
-    };
-    % - masks to be used to crop the images
-    maskPaths = fullfile(output_folder, 'ROCh', 'test', 'masks');
-    % crop!!
-    script_cropFOVSet;
+    % cropping test data set
+    if exist(test_input_dir, 'dir')==0
+        fprintf('Cropping test set data...\n');
+        % - path where the images to crop are
+        sourcePaths = { ...
+            fullfile(output_folder, 'ROCh', 'test', 'images'), ...
+            fullfile(output_folder, 'ROCh', 'test', 'masks'), ...
+        };
+        % - paths where the images to be cropped will be saved
+        outputPaths = { ...
+            fullfile(output_folder, 'ROCh', 'test', 'images'), ...
+            fullfile(output_folder, 'ROCh', 'test', 'masks'), ...    
+        };
+        % - masks to be used to crop the images
+        maskPaths = fullfile(output_folder, 'ROCh', 'test', 'masks');
+        % crop!!
+        script_cropFOVSet;
+    end
     
 end
